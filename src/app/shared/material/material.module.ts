e7a7e7727c68a34c46845eb8192fa8232313f84f
João Pedro <dp.joaopedro@gmail.com>
2022-05-16 14:17:04 -0300

    include toolbar

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const modules = [MatCardModule, MatButtonModule];
+const modules = [MatCardModule, MatButtonModule, MatToolbarModule];
 
 @NgModule({
   declarations: [],
